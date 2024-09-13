69377f2b3f7eae43f5dec43dc249078e999ddc33
Mr-Lein <151097135+Mr-Lein@users.noreply.github.com>
2024-09-13 08:07:05 -0600

    Update BasicTest.java

--- src/test/java/BasicTest.java
+++ src/test/java/BasicTest.java
@@ -1,1 +1,14 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class BasicTest {
+    @Test
+    public void testCodeRuns() {
+        try {
+            HelloWorld.main(new String[]{});
+        } catch (Exception e) {
+            fail("The code threw an exception: " + e.getMessage());
+        }
+    }
+}
 
